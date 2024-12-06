0686f2b601ed21628c948e41552cd56dc2e8039e
MinTuan2124 <166462832+MinTuanit@users.noreply.github.com>
2024-12-07 00:07:46 +0700

    Add models

---
+++ Models/Topping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLyRapChieuPhim.Models;
+
+public partial class Topping
+{
+    public int IdTp { get; set; }
+
+    public string? TenTp { get; set; }
+
+    public int? Gia { get; set; }
+    public string? HinhAnh { get; set; }
+
+    public virtual ICollection<Cttp> Cttps { get; set; } = new List<Cttp>();
+}
